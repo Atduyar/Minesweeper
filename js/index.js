8db599f9cfefaaa7863c448531d21f2bf69828bd
Ahmet Tarik Duyar(Atduyar) <atduyar@gmail.com>
2025-01-04 18:59:31 +0100

    fix: commented lines. add:some notes

--- js/index.js
+++ js/index.js
@@ -7,29 +7,7 @@ const screen = blessed.screen({
 	title: 'Minesweeper',
 });
 
-
 screen.menuScreen = new MenuScreen(screen);
 screen.gameScreen = new GameScreen(screen);
 screen.menuScreen.attach();
 screen.render();
-
-// let pro = blessed.prompt({
-// 	parent: screen,
-// 	border: 'line',
-// 	width: '50%',
-// 	height: '30%',
-// 	top: 'center',
-// 	left: 'center',
-// 	label: ' Enter a Number ',
-// 	tags: true,
-// 	keys: true,
-// 	vi: true,
-// });
-// pro.input('Enter your name:', '', (err, value) => {
-// 	if (!err) {
-// 		console.log(value);
-// 	}
-// });
-
-// screen.append(pro);
-// screen.append(box);
---
+++ js/notes.md
@@ -0,0 +1,16 @@
+## OpenCell/Reveal Method Flowchart
+1. Oyun bittyse birsey yapma.
+2. X Y verilmediyse cursoru kullan.
+3. Flagliysa birsey yapma.
+4. Zaten aciksa smartReveal. 
+5. the cell i ac.
+6. mayinsa ve ilk hamleyse mayini degistir.
+7. degilse gameover.
+8. sayisysa ve 0 sa autoReveal.
+9. oyun statlarini guncelle.
+
+## Classlar
+- GameSettings
+- Cell
+- Board
+- Main
